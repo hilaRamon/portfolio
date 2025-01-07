46d0244977dc90b88b1ac36e21bacae96c9deb5f
hilaRamon <165134171+hilaRamon@users.noreply.github.com>
2025-01-07 13:04:46 +0200

    Bulid the app structure

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Files from "./pages/Files";
+import Examples from "./pages/Examples";
 
 function App() {
   return (
     <>
       <p>My PortFolio</p>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/files" element={<Files />} />
+        <Route path="/examples" element={<Examples />} />
+      </Routes>
     </>
   );
 }
